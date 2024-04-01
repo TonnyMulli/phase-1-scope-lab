fde69a57da0ad502cd43321f631651381138280a
Tonny Mulli <tonnymulli2118@gmail.com>
2024-04-01 10:48:29 +0300

    Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'tony'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'laureen'
+}
 // Write your solution in this file!
